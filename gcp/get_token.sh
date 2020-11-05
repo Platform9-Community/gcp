a211d0c9ba77983f1c0f912ff3cca6b7bd559e99
snimje <surendra@platform9.com>
2020-11-05 16:26:13 +0530

    updated

--- gcp/get_token.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-source "${PWD}/common.sh"
-get_token
-echo ${X_AUTH_TOKEN}
-echo ${PROJECT_UUID}
--- gcp/kubectl-pmk45.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.17.0/bin/linux/amd64/kubectl
-sudo chown root:root kubectl
-sudo chmod a+x kubectl
-sudo mv kubectl /usr/local/bin
